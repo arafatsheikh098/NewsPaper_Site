21f2c4c8c580a670e2b60278e134523fb91a4a6e
arafatsheikh098 <arafat.csecu@gmail.com>
2025-04-24 14:12:32 +0600

    Homepage and category page added

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- news/views.py
+++ news/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Category
 
-# Create your views here.
+def home(request):
+    articles = Article.objects.all().order_by('-date_posted')
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'articles': articles, 'categories': categories})
+
+def category_articles(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    articles = Article.objects.filter(category=category).order_by('-date_posted')
+    categories = Category.objects.all()
+    return render(request, 'category_articles.html', {
+        'category': category,
+        'articles': articles,
+        'categories': categories
+    })
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'article_detail.html', {'article': article})
