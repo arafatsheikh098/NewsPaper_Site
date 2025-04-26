aa040aac7bdb5f67458b5e01b5f81e3ca266d7f0
arafatsheikh098 <arafat.csecu@gmail.com>
2025-04-26 13:52:53 +0600

    Navbar added

--- news/models.py
+++ news/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author_name = models.CharField(max_length=100)
     date_posted = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
